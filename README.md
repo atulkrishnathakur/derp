30ca77065e564e59307e37dda3f03cad368d42e0
Atul Krishna Thakur <gitvcs@gmail.com>
2023-10-18 10:02:02 +0530

    setting work

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-# derp
+# install mysqlclient
+```
+ATUL@DESKTOP-21EPCLH MINGW64 /i/mydjangofirst/derp (main)
+$ pip install mysqlclient
+Requirement already satisfied: mysqlclient in i:\mydjangofirst\venv\lib\site-packages (2.1.1)
+
+```
+# Requirements txt
+```
+ATUL@DESKTOP-21EPCLH MINGW64 /i/mydjangofirst/derp (main)
+$ python -m pip freeze > requirements.txt
+(venv) 
+```
--- derp/settings.py
+++ derp/settings.py
@@ -74,12 +74,18 @@ WSGI_APPLICATION = 'derp.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': {  
+        'ENGINE': 'django.db.backends.mysql',  
+        'NAME': 'derp',  
+        'USER':'root',  
+        'PASSWORD':'',  
+        'HOST':'localhost',  
+        'PORT':'3306',
+        'OPTIONS': {
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"
+        },  
+    }  
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
