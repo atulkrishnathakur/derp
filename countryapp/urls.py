494629191524a8c2cf12f7a17979ac2a68468a2d
Atul Krishna Thakur <atul77.cs@gmail.com>
2023-11-06 11:06:40 +0530

    add country

--- countryapp/urls.py
+++ countryapp/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.list, name='countries'),
     path('list', views.list, name='countrylist'),
+    path('create', views.create, name='countrycreate'),
+    path('savecountry', views.savecountry, name='savecountry'),
 ]
--- countryapp/views.py
+++ countryapp/views.py
@@ -1,5 +1,18 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from countryapp.models import Country 
 
 def list(request):
     return render(request,'countryapp/list.html')
+def create(request):
+    return render(request,'countryapp/create.html')
+def savecountry(request):
+    try:
+        if request.method == 'POST':
+            country_name = request.POST['countryname']
+            countryobj = Country(country_name=country_name)
+            countryobj.save()
+            return redirect('countrylist')
+    except Exception as e:
+        print(e)    
+
