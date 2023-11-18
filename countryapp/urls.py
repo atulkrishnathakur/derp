7129a6594931113bcb91bdb288afb1526969509a
Atul Krishna Thakur <atul77.cs@gmail.com>
2023-11-18 18:17:28 +0530

    validation added in country

--- countryapp/urls.py
+++ countryapp/urls.py
@@ -5,5 +5,5 @@ urlpatterns = [
     path('', views.list, name='countries'),
     path('list', views.list, name='countrylist'),
     path('create', views.create, name='countrycreate'),
-    path('savecountry', views.savecountry, name='savecountry'),
+    #path('savecountry', views.savecountry, name='savecountry'),
 ]
--- countryapp/views.py
+++ countryapp/views.py
@@ -1,18 +1,34 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-from countryapp.models import Country 
+from django.http import HttpResponse, HttpResponseRedirect
+from countryapp.models import Country
+from django.template import loader
+from django.shortcuts import redirect
+from django.urls import reverse
 
 def list(request):
     return render(request,'countryapp/list.html')
 def create(request):
-    return render(request,'countryapp/create.html')
-def savecountry(request):
     try:
+        if request.method == 'GET':
+            context = {}
+            return render(request,'countryapp/create.html',context)
         if request.method == 'POST':
+            errorlist = {}
             country_name = request.POST['countryname']
-            countryobj = Country(country_name=country_name)
-            countryobj.save()
-            return redirect('countrylist')
+            if country_name == "":
+                errorlist['country_name_err'] = 'Country name is required'
+            elif country_name.isnumeric():
+                errorlist['country_name_err'] = 'Enter correct country name'    
+            if(len(errorlist) != 0):
+                context = {
+                    'errorlist':errorlist
+                }
+                return render(request,'countryapp/create.html',context)
+            else:
+                countryobj = Country(country_name=country_name)
+                countryobj.save()
+                return HttpResponseRedirect(reverse("countrylist"))
     except Exception as e:
-        print(e)    
-
+        print(e)
+        context = {}
+        return render(request,'countryapp/create.html',context)
